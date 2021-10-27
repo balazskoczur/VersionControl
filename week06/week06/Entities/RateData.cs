97cd70c1ca9612a57f637568b22383205eddf3a4
Balázs Koczur <balazs.koczur@stud.uni-corvinus.hu>
2021-10-27 18:51:14 +0200

    04

---
+++ week06/week06/Entities/RateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week06.Entities
+{
+    class RateData
+    {
+        DateTime Date;
+        string currency;
+        decimal value;
+    }
+}
