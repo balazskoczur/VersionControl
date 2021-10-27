46eefccc641e337a6593b1847c49824777f6671d
Balázs Koczur <balazs.koczur@stud.uni-corvinus.hu>
2021-10-27 18:58:23 +0200

    05

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using week06.Entities;
 using week06.MnbServiceReference;
 
@@ -14,15 +15,15 @@ namespace week06
 {
     public partial class Form1 : Form
     {
-        BindingList<RateData> Rates;
+        BindingList<RateData> Rates = new BindingList<RateData>;
         public Form1()
         {
             InitializeComponent();
-            Arfolyamfuggveny();
+            Arfolyam();
             dataGridView1.DataSource = Rates;
+            xmlolvasas();
         }
-
-        private void Arfolyamfuggveny()
+        public void Arfolyam()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
@@ -34,6 +35,25 @@ namespace week06
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
         }
+        public void xmlolvasas()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0) rate.Value = value / unit;
+            }
+        }
+
+
     }
 
 }
